a8dd41986a178a6af18ff77c7dc11ae9d07301b0
Hacke Wayne <hacklew@hotmail.com>
2021-06-25 21:49:06 +1200

    with loader

--- cypress/integration/tigery.ts
+++ cypress/integration/tigery.ts
@@ -1,3 +1,5 @@
+import { transform } from "cypress/types/lodash";
+
 describe('tiger app', () => {
     it('can open', () => {
         cy.visit('');
@@ -9,8 +11,19 @@ describe('tiger app', () => {
 
         cy.url().should('eq', Cypress.config().baseUrl + 'photos/tiger/web');
 
-        cy.get('a').contains('Search for something else').click();
+        cy.get('img').should(img => expect(img[0].naturalWidth).to.be.greaterThan(0));
+        
+        cy.get('img').should('have.css', 'transform').should('eq', 'matrix(1, 0, 0, 1, 0, 0)');
 
-        cy.url().should('eq', Cypress.config().baseUrl + 'search');
+        cy.get('button').contains('Rotate').click();
+
+        cy.get('img').should('have.css', 'transform').should('not.eq', 'matrix(1, 0, 0, 1, 0, 0)');
+
+        // how to assert more rotation
+        cy.get('img').should('have.css', 'transform').then(rotatedOnce => {
+            cy.get('button').contains('Rotate').click();
+
+            cy.get('img').should('have.css', 'transform').should('not.eq', rotatedOnce);
+        });
     });
 });
